405616c00ba0d03ad8ba49a912544cb6b17c2739
bsavanth <39973983+bsavanth@users.noreply.github.com>
2020-12-23 16:58:41 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/savanth/Demo/controllers/HelloController.java
@@ -0,0 +1,77 @@
+package org.launchcode.savanth.Demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+
+public class HelloController {
+
+
+    @GetMapping("form")
+    public String helloForm()
+    {
+        return "form";
+
+    }
+
+   @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}, value="hello")
+
+    public String helloWithTwoQueryParam(@RequestParam String name, String name1, Model model)
+    {
+        ArrayList<String> greetings = new ArrayList<>();
+        greetings.add("hello");
+        greetings.add("Bonjour");
+        greetings.add("Kaise ho");
+        greetings.add("Ela unnav");
+        greetings.add("Kasa ho");
+
+        model.addAttribute("greetings",greetings);
+        model.addAttribute("name", name);
+
+//        String holdHEllos[] ={"Hello", "Bonjour", "Kaise ho", "Ela unnav", "Kasa ho"};
+
+
+//        if(name1.equals("english"))
+//        {
+//            model.addAttribute("greeting",holdHellos[0]);
+//            model.addAttribute("name",name);
+//        }
+//
+//        else if(name1.equals("french"))
+//        {
+//            model.addAttribute("greeting",holdHellos[1]);
+//            model.addAttribute("name",name);
+//        }
+//
+//        else if(name1.equals("hindi"))
+//        {
+//            model.addAttribute("greeting",holdHellos[2]);
+//            model.addAttribute("name",name);
+//        }
+//
+//        else if(name1.equals("telugu"))
+//        {
+//            model.addAttribute("greeting",holdHellos[3]);
+//            model.addAttribute("name",name);
+//        }
+//
+//        else if(name1.equals("marathi"))
+//        {
+//            model.addAttribute("greeting",holdHellos[4]);
+//            model.addAttribute("name",name);
+//        }
+
+        return "respond";
+    }
+
+
+
+
+}
